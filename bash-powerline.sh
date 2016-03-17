8925267aa32cb2256a95f8be666ea17019e1010a
Arnold Cano <arnoldcano@yahoo.com>
2016-03-17 17:10:00 -0500

    Removed extra statuses

--- bash-powerline.sh
+++ bash-powerline.sh
@@ -12,8 +12,6 @@ __powerline() {
     readonly GIT_NEED_PULL_SYMBOL='⇣'
     readonly DOCKER_SYMBOL='☐ '
     readonly DOCKER_RUNNING_SYMBOL='⇡'
-    readonly DOCKER_STOPPING_SYMBOL='⇤'
-    readonly DOCKER_STARTING_SYMBOL='⇥'
     readonly DOCKER_ERROR_SYMBOL='*'
     readonly DOCKER_STOPPED_SYMBOL='⇣'
 
@@ -101,12 +99,6 @@ __powerline() {
             case $status in
                 Running)
                     status=" $DOCKER_RUNNING_SYMBOL"
-                    ;;
-                Stopping)
-                    status=" $DOCKER_STOPPING_SYMBOL"
-                    ;;
-                Starting)
-                    status=" $DOCKER_STARTING_SYMBOL"
                     ;;
                 Error|Timeout)
                     status=" $DOCKER_ERROR_SYMBOL"
